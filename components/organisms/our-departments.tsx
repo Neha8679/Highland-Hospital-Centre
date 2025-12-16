0e456a2e397500216d6805b7983b2f92bca13013
Neha8679 <safercode067@gmail.com>
2025-12-17 00:33:46 +0530

    home page done with data from db

--- components/organisms/our-departments.tsx
+++ components/organisms/our-departments.tsx
@@ -1,20 +1,40 @@
-import { departmentData } from "@/db/dummydata";
+// import { departmentData } from "@/db/dummydata";
 import DepartmentCard from "../molecules/departmentcard";
 import { Divide } from "lucide-react";
+import { getDepartments } from "@/lib/actions/settings.actions";
+import { DepartmentData } from "@/types";
  
-interface DepartmentData {
-  id: string;
-  name: string;
-  iconName: string;
-}
+// interface DepartmentData {
+//   id: string;
+//   name: string;
+//   iconName: string;
+// }
  
-export default function DepartmentsSection() {
-  const departments: DepartmentData[] = departmentData;
+export default async function DepartmentsSection() {
+  let departments: DepartmentData[] = [];
+  let fetchError: string | null = null;
+  try {
+    const result = await getDepartments();
+    if (result.success && result.data) {
+      departments = result.data.departments;
+      //departments = [];
+    } else {
+      fetchError = result.message || "Failed to load departments";
+    }
+  } catch (error) {
+    const message =
+      error instanceof Error ? error.message : "An Unknown error occured";
+    fetchError = message;
+  }
+ 
+  // const departments: DepartmentData[] = departmentData;
  
   return (
     <section className="w-full">
       <h2 className="text-center text-text-title mb-8">Our Departments</h2>
-      {departments.length === 0 ? (
+      {fetchError ? (
+        <div className="text-center text-red-500 py-4">{fetchError}</div>
+      ) : departments.length === 0 ? (
         <div className="text-grey-500 py-4 text-center">
           No deparments found
         </div>
@@ -33,3 +53,4 @@ export default function DepartmentsSection() {
     </section>
   );
 }
+ 
